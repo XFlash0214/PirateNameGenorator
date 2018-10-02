4d6108b31a686c5079aef9874490ed70801ce8c8
Sampson Neyman <43590651+XFlash0214@users.noreply.github.com>
2018-10-02 18:47:26 -0500

    Add files via upload

--- Pirate Name.py
+++ Pirate Name.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import random
 class piratename():
     originalfirst=""
@@ -13,5 +14,33 @@ class piratename():
         x=random.randint(0,len(self.firstlist)-1)
         y=random.randint(0,len(self.lastlist)-1)
         return self.firstlist[x]+self.lastlist[y]
+root=Tk()
+root.title("Pirate Name Genorator")
+myfont="Arial 20 bold"
+banner=PhotoImage(file="pirate-banner.gif")
+banner=banner.subsample(2,2)
+
+title=Label(root,text="What's e'Pirate Name?",font="Arial 30 bold")
+title.grid(row=0,column=0,columnspan=2)
+
+flabel=Label(root,text="First Name",font=myfont)
+flabel.grid(row=1,column=0)
+
+llabel=Label(root,text="Last Name",font=myfont)
+llabel.grid(row=2,column=0)
+
+ftext=Entry(root,font=myfont)
+ftext.grid(row=1,column=1)
+
+ltext=Entry(root,font=myfont)
+ltext.grid(row=2,column=1)
+
+bshow=Button(root,text="Finish",font=myfont)
+bshow.grid(row=3,column=0,columnspan=2)
+
+output=Label(root,font="Script 30 bold",image=banner)
+output.grid(row=4,column=0,columnspan=2)
+
 mypirate=piratename("Sampson ", "Neyman ")
 print(mypirate.createname())
+root.mainloop()

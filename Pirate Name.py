e881b62aa4f908fdac3360411d7a8ba40210cb6a
Sampson Neyman <43590651+XFlash0214@users.noreply.github.com>
2018-10-02 19:13:09 -0500

    Add files via upload

--- Pirate Name.py
+++ Pirate Name.py
@@ -3,10 +3,8 @@ import random
 class piratename():
     originalfirst=""
     originallast=""
-
     firstlist=["Cap'n Fitz ","Scallywagger ", "Stupid Face ", "Blabby "]
     lastlist=["Sharkyway","Nibble","Footy","Queeker"]
-
     def __init__(self,firstName,lastName):
         self.originalfirst=firstName
         self.originallast=lastName
@@ -14,6 +12,15 @@ class piratename():
         x=random.randint(0,len(self.firstlist)-1)
         y=random.randint(0,len(self.lastlist)-1)
         return self.firstlist[x]+self.lastlist[y]
+def showname():
+    fn=ftext.get()
+    ln=ltext.get()
+    ftext.delete(0,"end")
+    ltext.delete(0,"end")
+    mygen=piratename(fn,ln)
+    pirateoutput=mygen.createname()
+    output.config(text=pirateoutput,compound=CENTER)
+    
 root=Tk()
 root.title("Pirate Name Genorator")
 myfont="Arial 20 bold"
@@ -35,7 +42,7 @@ ftext.grid(row=1,column=1)
 ltext=Entry(root,font=myfont)
 ltext.grid(row=2,column=1)
 
-bshow=Button(root,text="Finish",font=myfont)
+bshow=Button(root,text="Finish",font=myfont,command=showname,bg="red")
 bshow.grid(row=3,column=0,columnspan=2)
 
 output=Label(root,font="Script 30 bold",image=banner)
